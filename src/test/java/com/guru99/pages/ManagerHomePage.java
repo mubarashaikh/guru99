e063fd922fcf963fa056403822b2295c2b205e8a
abdieid <jibriljama1211@gmail.com>
2020-05-19 18:52:38 +0100

    Live project day1 implementation

--- src/test/java/com/guru99/pages/ManagerHomePage.java
+++ src/test/java/com/guru99/pages/ManagerHomePage.java
@@ -1,10 +1,25 @@
 package com.guru99.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
 
 public class ManagerHomePage extends BasePage {
    public ManagerHomePage (WebDriver driver) {
        this.driver = driver;
 
    }
+@FindBy(css = "body > div:nth-child(6) > h2")
+    private WebElement pageTitle;
+
+   public void userSuccessfullyLogin (){
+        String pageTit = "Guru99 Bank";
+       Assert.assertTrue(pageTitle.getText().equals(pageTit));
+   }
+
+
+
+
 }
