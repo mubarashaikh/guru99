08f001553c508a0fa388a97eb9a5abcdd18e85ec
abdieid <jibriljama1211@gmail.com>
2020-05-22 20:19:25 +0100

    new changes

--- src/test/java/com/guru99/pages/BasePage.java
+++ src/test/java/com/guru99/pages/BasePage.java
@@ -1,6 +1,13 @@
 package com.guru99.pages;
 
 import com.guru99.commons.Driver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 
 public class BasePage extends Driver {
 
@@ -10,5 +17,30 @@ public class BasePage extends Driver {
 
         driver.navigate().to(BASE_URL);
     }
+
+    /*##########################################################################
+	Uses - This method helps us to take screen shots of failed tests
+	############################################################################*/
+
+    public static File takeScreenShot() throws Exception
+    {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    }
+    /*##########################################################################
+       Uses - This method helps us to save screen shots of failed tests
+    ############################################################################*/
+    public static void saveScreenshot() throws Exception
+    {
+        String dateNow = new SimpleDateFormat("ddMMyy").format(new GregorianCalendar().getTime());
+        String timeNow = new SimpleDateFormat("hhmmss").format(new GregorianCalendar().getTime());
+
+        String fileName = String.format(".\\Screenshots\\%s\\screenshot_%s", dateNow, timeNow);
+
+        File screenshot = takeScreenShot();
+
+        FileUtils.copyFile(screenshot, new File(fileName));
+
+    }
+
 
 }
