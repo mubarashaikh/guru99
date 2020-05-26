937a06cc52688417886fc37a81edee14bb04f47d
abdieid <jibriljama1211@gmail.com>
2020-05-27 00:05:49 +0100

    new changes

--- src/test/java/com/guru99/hooks/Hook.java
+++ src/test/java/com/guru99/hooks/Hook.java
@@ -1,13 +1,13 @@
 package com.guru99.hooks;
 
 import com.guru99.commons.Browsers;
+
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import io.cucumber.core.api.Scenario;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
 
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
@@ -26,23 +26,43 @@ public class Hook  extends Browsers {
     @After
     public void tearDown()
     {
-        String dateNow = new SimpleDateFormat("dd/MM/yyyy")
-                .format(new GregorianCalendar().getTime());
-        String timeNow = new SimpleDateFormat("HH:mm:ss")
-                .format(new GregorianCalendar().getTime());
-        String filename = String.format("./Screenshots/%s/screenshot_%s.png", dateNow, timeNow);
+//        String dateNow = new SimpleDateFormat("dd/MM/yyyy")
+//                .format(new GregorianCalendar().getTime());
+//        String timeNow = new SimpleDateFormat("HH:mm:ss")
+//                .format(new GregorianCalendar().getTime());
+//
+//        // the problem is here it will not take a formatted string
+//        //String filename = String.format("./Screenshots/%s/screenshot_%s/png", dateNow, timeNow);
+//
+//        if(scenario.isFailed())
+//        {
+//            try{
+//                byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+//                scenario.embed(screenshot, "image/png" );
+//
+//            } catch (Exception e)
+//            {
+//                e.printStackTrace();
+//            }
+//        }
 
-        if(scenario.isFailed())
-        {
-            try{
-                byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-                scenario.embed(screenshot, filename);
-            } catch (Exception e)
-            {
-                e.printStackTrace();
+
+        System.out.println("AFTER SITE");
+
+        try {
+
+            if(scenario.isFailed()) {
+                scenario.write("this is my failure message");
+                TakesScreenshot ts = ((TakesScreenshot) driver);
+                byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+
+                scenario.embed(screenshot, "image/png");
             }
         }
+        catch(Exception e) {
 
+            e.printStackTrace();
+        }
 
         closeBrowser();
     }
--- src/test/java/com/guru99/runners/TestRunner.java
+++ src/test/java/com/guru99/runners/TestRunner.java
@@ -1,17 +1,43 @@
 package com.guru99.runners;
 
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
         features = {"src/test/java/com/guru99/features"},
         glue = {"com/guru99/hooks","com/guru99/stepDefinitions"},
-        plugin = {"pretty", "json:target/report.json",
-                "de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber"}
+        plugin = { "pretty","json:target/cucumber.json",
+                "de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        }
+
 )
 public class TestRunner {
+
+//        //Writing the Extent Reports:
+//        //Add a method writeExtentReport()
+//        @AfterClass
+//        public static void setUp() {
+//
+//
+////                Reporter.loadXMLConfig(new File("/bankguru99/Configuration/extent-config.xml"));
+////                Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+////                Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+////                Reporter.setSystemInfo("Machine", "Windows 10" + "64 Bit");
+////                Reporter.setSystemInfo("Selenium", "3.7.0");
+////                Reporter.setSystemInfo("Maven", "3.5.2");
+////                Reporter.setSystemInfo("Java Version", "1.8.0_151");
+//        }
+
+
 }
